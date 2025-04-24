9340d602bc41b15f097c4afe9ee106e830d29cbf
Habib <habibleo861@gmail.com>
2025-04-24 12:28:50 +0600

    Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,8 @@
+
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchProducts } from './../producslice';
-import ProductCard from './ProductCard';
+import ProductCard from '../components/ProductCard';
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -11,13 +12,13 @@ const Home = () => {
     if (status === 'idle') dispatch(fetchProducts());
   }, [status, dispatch]);
 
-  if (status === 'loading') return <p>Loading...</p>;
-  if (status === 'failed') return <p className="text-red-600">{error}</p>;
+  if (status === 'loading') return <p className="text-center p-4">Loading...</p>;
+  if (status === 'failed') return <p className="text-center text-red-500 p-4">{error}</p>;
 
   return (
-    <div>
-      <h1 className="text-2xl font-bold mb-4">Product List</h1>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-x-40 gap-y-6">
+    <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
+      <h1 className="text-2xl sm:text-3xl font-bold my-6 text-center">Product List</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3  gap-6">
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
